b838fd9c4cb51467b7c3ba23abc1dc8e51195b64
Julio Dourado <juliodourado419@gmail.com>
2025-06-11 10:33:27 -0300

    refactor(conftest): retirando imports e comentarios

--- backend/app/test/conftest.py
+++ backend/app/test/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.db.db import Base, get_db
+from app.db.db import Base
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,16 +12,14 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Criação das tabelas no banco de dados de teste
 @pytest.fixture(scope="module")
 def setup_db():
     
     Base.metadata.create_all(bind=engine)
-    yield TestingSessionLocal()  # Fornece uma sessão para os testes
+    yield TestingSessionLocal()
     # Limpa as tabelas após os testes
     Base.metadata.drop_all(bind=engine)
 
-# Fixture para obter a sessão do banco de dados
 @pytest.fixture
 def db_session(setup_db):
     return setup_db
